eeea4b4797c1d709ad9d029a1b5c0f83b71a8dd6
Adam Lyon <lyon@fnal.gov>
2020-10-10 23:44:30 -0500

    Added more testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,28 @@
 using IRMA
 
 using Test
-using OnlineStats: Hist
+using OnlineStats: Hist, fit!
 
 @testset "SHist" begin
 
+    
     h = fit!(Hist(-5:0.2:5), randn(1000))
-
     sh = SHist(h)
-
-    @test sh isa SHist
-    @test isbits(sh)   # This is the important one
-    @test sh.counts == h.counts
-    @test sh.out == h.out
-    @test sh.edges == h.edges
-
     hh = Hist(sh)
 
-    @test hh isa OnlineStats.Hist
-    @test hh.counts == h.counts
-    @test hh.out == h.out
-    @test hh.edges == h.edges
+    @testset "HistToSHist" begin
+        @test sh isa SHist
+        @test isbits(sh)   # This is the important one
+        @test sh.counts == h.counts
+        @test sh.out == h.out
+        @test sh.edges == h.edges
+    end
+
+    @testset "SHistToHist" begin
+        @test hh isa Hist
+        @test hh.counts == h.counts
+        @test hh.out == h.out
+        @test hh.edges == h.edges
+    end 
 
 end
