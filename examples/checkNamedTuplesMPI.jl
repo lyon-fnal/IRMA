4dd6edca28beb7230efddba44a1bcb12ea4dcae8
Adam Lyon <lyon@fnal.gov>
2020-10-13 23:40:24 -0500

    Do NamedTuples work in MPI transfers Fixes #6

---
+++ examples/checkNamedTuplesMPI.jl
@@ -0,0 +1,29 @@
+# Check that NamedTuples work for MPI.Gather
+# This answers issue 6 https://github.com/lyon-fnal/IRMA/issues/6
+using MPI
+
+# -- MPI boilerplate --
+MPI.Init()
+
+const info = MPI.Info()
+const comm = MPI.COMM_WORLD
+
+const nprocs = MPI.Comm_size(comm)
+const myrank = MPI.Comm_rank(comm)
+const root = 0
+const isroot = myrank == root
+
+function go()
+
+    # Make a NamedTuple with some data
+    nt = (myrank=myrank, nprocs=nprocs)
+    @show nt
+
+    allNT = MPI.Gather(nt, root, comm)
+    if isroot
+        @show allNT
+    end
+
+end
+
+go()
