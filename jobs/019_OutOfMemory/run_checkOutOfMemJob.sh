0b28aaaf403f00039321642d114041457a700962
Adam Lyon <lyon@fnal.gov>
2020-10-24 09:13:11 -0700

    Updates for running vtune

--- jobs/019_OutOfMemory/run_checkOutOfMemJob.sh
+++ jobs/019_OutOfMemory/run_checkOutOfMemJob.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #SBATCH --qos=debug
-#SBATCH --time=5
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=2
 #SBATCH --constraint=haswell
+#SBATCH --perf=vtune
+
+module load vtune
 
 export JULIA_DEBUG="outOfMemJob,IRMA"
-srun julia --project=$HOME/IRMA/jobs/019_OutOfMemory outOfMemJob.jl
+vtune -finalization-mode=deferred -collect memory-consumption -r $PWD/vtune_mem srun julia --project=$HOME/IRMA/jobs/019_OutOfMemory outOfMemJob.jl
