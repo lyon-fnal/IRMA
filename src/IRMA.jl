34e47af308c878c5122aea619a8eeb5752a00d7b
Adam Lyon <lyon@fnal.gov>
2020-10-27 01:32:59 -0500

    Analyze results from Strong Scaling jobs Fixes #18

--- src/IRMA.jl
+++ src/IRMA.jl
@@ -2,13 +2,14 @@ module IRMA
 
 using OnlineStats, OnlineStatsBase
 using StaticArrays
+using Pipe
 using MPI
 import Base: merge
 import Distributed: splitrange
 
 export
     SHist, nobs, value, Hist, merge, mergeStatsCollectionWithSHist, 
-    partitionDS, Stopwatch, stamp, asNamedTuple
+    partitionDS, Stopwatch, stamp, asNamedTuple, rankTimings, rankTotalTime
 
 include("shist.jl")
 include("partitionDS.jl")
